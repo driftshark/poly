344b013a4bcb106be70c11b72836637ad4fc8967
suyoin <19378617+suyoin@users.noreply.github.com>
2021-10-04 23:15:12 -0500

    hot reload system

---
+++ src/systems/hot.ts
@@ -0,0 +1,54 @@
+import createSystem from "createSystem";
+import { UpdateSystem } from "System";
+
+export = createSystem(() => {
+	return game.GetService("RunService").IsStudio()
+		? {
+				name: "hot",
+				init: (world) => {
+					task.defer(() => {
+						for (const [trackedSystemInstance, system] of world["systems"]) {
+							if (!typeIs(trackedSystemInstance, "Instance")) {
+								continue;
+							}
+
+							(trackedSystemInstance.Changed as RBXScriptSignal).Connect(() => {
+								print(`hmr update - ${system.name}`);
+
+								let thisSystemInstance: ModuleScript | undefined = undefined;
+								for (const [k, v] of pairs(world["systems"])) {
+									if (v.name === system.name) {
+										thisSystemInstance = k as ModuleScript;
+										break;
+									}
+								}
+
+								if (thisSystemInstance !== undefined) {
+									const currentSystem =
+										world["systems"].get(thisSystemInstance)!;
+									if (currentSystem.destroy) currentSystem.destroy(world);
+
+									world["systems"].delete(thisSystemInstance);
+									const index = world["updateSystems"].indexOf(
+										currentSystem as UpdateSystem
+									);
+									if (index !== -1) {
+										world["updateSystems"].remove(index);
+									}
+
+									world.registerSystems([
+										(
+											require(trackedSystemInstance.Clone()) as ReturnType<
+												typeof import("../createSystem").default
+											>
+										)(),
+									]);
+								}
+							});
+						}
+					});
+				},
+				destroy: () => {},
+		  }
+		: { name: "hot" };
+});
