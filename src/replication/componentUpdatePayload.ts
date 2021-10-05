8611b23e931c9f1ffcc2621c3d3270e7711e938d
suyoin <19378617+suyoin@users.noreply.github.com>
2021-10-05 18:10:47 -0500

    utilities for getting and patching replication payloads

---
+++ src/replication/componentUpdatePayload.ts
@@ -0,0 +1,105 @@
+import { World } from "index";
+import { ReplicationType } from "replication";
+import { named } from "util/symbol";
+import { diff, patch, shallow } from "util/tableUtil";
+
+export const getPayload = <
+	TComponentName extends keyof Components,
+	TKey extends keyof Components[TComponentName]["data"]
+>(
+	world: World,
+	componentName: TComponentName,
+	oldValue: Components[TComponentName]["data"],
+	newValue: Components[TComponentName]["data"]
+): symbol | Map<unknown, unknown> | Components[TComponentName]["data"] => {
+	//@ts-ignore
+	const replicateDefinition = world["componentDefinitions"][componentName][
+		"replicate"
+	] as
+		| ReplicationType
+		| {
+				[key in TKey]?: ReplicationType;
+		  };
+
+	if (typeIs(replicateDefinition, "number")) {
+		if (replicateDefinition === ReplicationType.Diff) {
+			return diff(oldValue, newValue);
+		} else {
+			return newValue;
+		}
+	} else {
+		//check changed fields that are included in the replicateDefinition
+		const payload = new Map();
+
+		for (const [key, keyReplicationType] of pairs(replicateDefinition)) {
+			if (oldValue[key as TKey] === newValue[key as TKey]) {
+				continue;
+			}
+
+			if (keyReplicationType === ReplicationType.Diff) {
+				payload.set(
+					key,
+					diff(
+						oldValue[key as TKey] as unknown as object,
+						newValue[key as TKey] as unknown as object
+					)
+				);
+			} else {
+				if (newValue[key as TKey] === undefined) {
+					payload.set(key, "_N");
+				} else {
+					payload.set(key, newValue[key as TKey]);
+				}
+			}
+		}
+
+		//if payload is empty, return None, as in no changes
+		return payload.isEmpty() ? named("None") : payload;
+	}
+};
+
+export const patchPayload = <
+	TComponentName extends keyof Components,
+	TKey extends keyof Components[TComponentName]["data"]
+>(
+	world: World,
+	componentName: TComponentName,
+	oldValue: Components[TComponentName]["data"],
+	payload: Components[TComponentName]["data"]
+) => {
+	//@ts-ignore
+	const replicateDefinition = world["componentDefinitions"][componentName][
+		"replicate"
+	] as
+		| ReplicationType
+		| {
+				[key in TKey]?: ReplicationType;
+		  };
+
+	if (typeIs(replicateDefinition, "number")) {
+		if (replicateDefinition === ReplicationType.Diff) {
+			return patch(oldValue, payload);
+		} else {
+			return payload;
+		}
+	} else {
+		const newValue = shallow(oldValue);
+
+		for (const [key, value] of pairs(payload)) {
+			if (replicateDefinition[key as TKey] === ReplicationType.Diff) {
+				//@ts-ignore
+				newValue[key] = patch(oldValue[key], value);
+			} else {
+				if (value === "_N") {
+					//@ts-ignore
+					newValue[key] = undefined;
+				} else {
+					//@ts-ignore
+					newValue[key] = value;
+				}
+			}
+		}
+
+		return newValue;
+	}
+};
