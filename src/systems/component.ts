91862c56c30d5100944746e9d3bdc2ad44ee8e4b
suyoin <19378617+suyoin@users.noreply.github.com>
2021-10-05 10:58:39 -0500

    cleanup on destroy

--- src/systems/component.ts
+++ src/systems/component.ts
@@ -3,15 +3,19 @@ import createSystem from "createSystem";
 //listens to CollectionService events for Instances with the Component tag for cleanup
 
 export = createSystem(() => {
+	let cn: RBXScriptConnection | undefined;
 	return {
 		name: "component",
 		onRegistered: (world) => {
-			game
+			cn = game
 				.GetService("CollectionService")
 				.GetInstanceRemovedSignal("Component")
 				.Connect((instance) => {
 					world.removeRef(instance);
 				});
+		},
+		destroy: () => {
+			cn?.Disconnect();
 		},
 	};
 });
