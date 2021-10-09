23d3f2ca5f43813bba7f3bc01083ae4710ac36af
suyoin <19378617+suyoin@users.noreply.github.com>
2021-10-08 19:20:25 -0500

    move receiver functions to a separate file

---
+++ src/replication/client/createClientUtilities.ts
@@ -0,0 +1,145 @@
+import { patchPayload } from "replication/componentUpdatePayload";
+import {
+	BulkCreateEvent,
+	BulkRemoveEvent,
+	BulkRemoveEventComponentToDescription,
+	BulkType,
+	CreateEvent,
+	RemoveEvent,
+	UpdateEvent,
+} from "replication/events";
+import type { World } from "world";
+
+type CallbackOf<T extends RemoteEvent> = T extends RemoteEvent<infer C>
+	? C
+	: never;
+
+type ConsumePayloadFunction =
+	| ((
+			world: World,
+			ref: Ref,
+			componentName: Components[keyof Components]["name"],
+			payload: Components[keyof Components]["data"]
+	  ) => boolean)
+	| undefined;
+
+export = (world: World) => {
+	const componentDefinitions = world["componentDefinitions"];
+
+	const receiveCreateEvent: CallbackOf<CreateEvent> = (
+		ref,
+		componentName,
+		data
+	) => {
+		//@ts-ignore
+		const consumePayload = componentDefinitions[componentName][
+			"consumePayload"
+		] as ConsumePayloadFunction;
+
+		if (consumePayload !== undefined) {
+			if (consumePayload(world, ref, componentName, data) === true) {
+				return;
+			}
+		}
+
+		world.addComponent(ref, componentName, data);
+	};
+
+	const receiveUpdateEvent: CallbackOf<UpdateEvent> = (
+		ref,
+		componentName,
+		payload
+	) => {
+		type ComponentData = Components[typeof componentName]["data"];
+
+		const oldValue = world.getComponent(ref, componentName) as
+			| DeepWritable<ComponentData>
+			| undefined;
+
+		const newData = patchPayload(world, componentName, oldValue, payload);
+
+		//@ts-ignore
+		const consumePayload = componentDefinitions[componentName][
+			"consumePayload"
+		] as ConsumePayloadFunction;
+
+		if (consumePayload !== undefined) {
+			if (consumePayload(world, ref, componentName, newData) === true) {
+				return;
+			}
+		}
+
+		world.addComponent(ref, componentName, newData);
+		world.fireEvent(
+			ref,
+			componentName,
+			"Replicated",
+			newData as DeepReadonly<ComponentData>,
+			oldValue as DeepReadonly<typeof oldValue>
+		);
+	};
+
+	const receiveRemoveEvent: CallbackOf<RemoveEvent> = (ref, componentName) => {
+		world.removeComponent(ref, componentName);
+	};
+
+	const receiveBulkCreateEvent: CallbackOf<BulkCreateEvent> = (
+		descriptions
+	) => {
+		for (const [componentName, descriptionArray] of pairs(descriptions)) {
+			//@ts-ignore
+			const consumePayload = componentDefinitions[componentName][
+				"consumePayload"
+			] as ConsumePayloadFunction;
+
+			if (consumePayload !== undefined) {
+				for (const description of descriptionArray) {
+					if (
+						consumePayload(
+							world,
+							description[0],
+							componentName,
+							description[1]
+						) === true
+					) {
+						continue;
+					}
+
+					world.addComponent(description[0], componentName, description[1]);
+				}
+			} else {
+				for (const description of descriptionArray) {
+					world.addComponent(description[0], componentName, description[1]);
+				}
+			}
+		}
+	};
+
+	const receiveBulkRemoveEvent: CallbackOf<BulkRemoveEvent> = (
+		bulkType,
+		descriptions,
+		ref
+	) => {
+		if (bulkType === BulkType.RefToComponents) {
+			for (const componentName of descriptions) {
+				world.removeComponent(ref, componentName);
+			}
+		} else if (bulkType === BulkType.ComponentToDescription) {
+			for (const [componentName, refArray] of pairs(
+				descriptions as Parameters<BulkRemoveEventComponentToDescription>[1]
+			)) {
+				for (const ref of refArray) {
+					world.removeComponent(ref, componentName);
+				}
+			}
+		}
+	};
+
+	return {
+		receiveCreateEvent,
+		receiveUpdateEvent,
+		receiveRemoveEvent,
+		receiveBulkCreateEvent,
+		receiveBulkRemoveEvent,
+	};
+};
--- src/systems/replication/clientSystem.ts
+++ src/systems/replication/clientSystem.ts
@@ -1,25 +1,13 @@
 import createSystem from "createSystem";
-import { World } from "index";
-import { patchPayload } from "replication/componentUpdatePayload";
+import createClientUtilities from "replication/client/createClientUtilities";
 import promiseChild from "util/promiseChild";
 import {
 	BulkCreateEvent,
 	BulkRemoveEvent,
-	BulkRemoveEventComponentToDescription,
-	BulkType,
 	CreateEvent,
 	RemoveEvent,
 	UpdateEvent,
 } from "../../replication/events";
-
-type ConsumePayloadFunction =
-	| ((
-			world: World,
-			ref: Ref,
-			componentName: Components[keyof Components]["name"],
-			payload: Components[keyof Components]["data"]
-	  ) => boolean)
-	| undefined;
 
 export = createSystem(() => {
 	let cns: RBXScriptConnection[] = [];
@@ -48,128 +36,22 @@ export = createSystem(() => {
 					const bulkCreateEvent = remotes[3] as BulkCreateEvent;
 					const bulkRemoveEvent = remotes[4] as BulkRemoveEvent;
 
+					const {
+						receiveCreateEvent,
+						receiveUpdateEvent,
+						receiveRemoveEvent,
+						receiveBulkCreateEvent,
+						receiveBulkRemoveEvent,
+					} = createClientUtilities(world);
+
+					cns.push(createEvent.OnClientEvent.Connect(receiveCreateEvent));
+					cns.push(updateEvent.OnClientEvent.Connect(receiveUpdateEvent));
+					cns.push(removeEvent.OnClientEvent.Connect(receiveRemoveEvent));
 					cns.push(
-						createEvent.OnClientEvent.Connect((ref, componentName, data) => {
-							//@ts-ignore
-							const consumePayload = world["componentDefinitions"][
-								componentName
-							]["consumePayload"] as ConsumePayloadFunction;
-
-							if (consumePayload !== undefined) {
-								if (consumePayload(world, ref, componentName, data) === true) {
-									return;
-								}
-							}
-
-							world.addComponent(ref, componentName, data);
-						})
+						bulkCreateEvent.OnClientEvent.Connect(receiveBulkCreateEvent)
 					);
-
 					cns.push(
-						updateEvent.OnClientEvent.Connect((ref, componentName, payload) => {
-							type ComponentData = Components[typeof componentName]["data"];
-
-							const oldValue = world.getComponent(ref, componentName) as
-								| DeepWritable<ComponentData>
-								| undefined;
-
-							const newData = patchPayload(
-								world,
-								componentName,
-								oldValue,
-								payload
-							);
-
-							//@ts-ignore
-							const consumePayload = world["componentDefinitions"][
-								componentName
-							]["consumePayload"] as ConsumePayloadFunction;
-
-							if (consumePayload !== undefined) {
-								if (
-									consumePayload(world, ref, componentName, newData) === true
-								) {
-									return;
-								}
-							}
-
-							world.addComponent(ref, componentName, newData);
-							world.fireEvent(
-								ref,
-								componentName,
-								"Replicated",
-								newData as DeepReadonly<ComponentData>,
-								oldValue as DeepReadonly<typeof oldValue>
-							);
-						})
-					);
-
-					cns.push(
-						removeEvent.OnClientEvent.Connect((ref, componentName) => {
-							world.removeComponent(ref, componentName);
-						})
-					);
-
-					cns.push(
-						bulkCreateEvent.OnClientEvent.Connect((descriptions) => {
-							for (const [componentName, descriptionArray] of pairs(
-								descriptions
-							)) {
-								//@ts-ignore
-								const consumePayload = world["componentDefinitions"][
-									componentName
-								]["consumePayload"] as ConsumePayloadFunction;
-
-								if (consumePayload !== undefined) {
-									for (const description of descriptionArray) {
-										if (
-											consumePayload(
-												world,
-												description[0],
-												componentName,
-												description[1]
-											) === true
-										) {
-											continue;
-										}
-
-										world.addComponent(
-											description[0],
-											componentName,
-											description[1]
-										);
-									}
-								} else {
-									for (const description of descriptionArray) {
-										world.addComponent(
-											description[0],
-											componentName,
-											description[1]
-										);
-									}
-								}
-							}
-						})
-					);
-
-					cns.push(
-						bulkRemoveEvent.OnClientEvent.Connect(
-							(bulkType, descriptions, ref) => {
-								if (bulkType === BulkType.RefToComponents) {
-									for (const componentName of descriptions) {
-										world.removeComponent(ref, componentName);
-									}
-								} else if (bulkType === BulkType.ComponentToDescription) {
-									for (const [componentName, refArray] of pairs(
-										descriptions as Parameters<BulkRemoveEventComponentToDescription>[1]
-									)) {
-										for (const ref of refArray) {
-											world.removeComponent(ref, componentName);
-										}
-									}
-								}
-							}
-						)
+						bulkRemoveEvent.OnClientEvent.Connect(receiveBulkRemoveEvent)
 					);
 				});
 		},
