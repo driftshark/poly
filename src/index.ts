1cf2add453621cb5b8c8f0198630adea590c62b3
suyoin <19378617+suyoin@users.noreply.github.com>
2021-10-12 18:04:05 -0500

    export Ref type and RefOf<ComponentName> type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { t } from "@rbxts/t";
+
 export { ComponentDefinition } from "./Component";
 export * from "./defineComponent";
 export { default as defineComponent } from "./defineComponent";
@@ -11,6 +13,14 @@ export { symbol, None };
 export { ReplicationType } from "./replication";
 
 import replicationMod from "./replication/replicationMod";
+
 export { replicationMod };
 export * from "./componentUtils/layered";
 export * from "./componentUtils/mapped";
+
+type eRef = Ref;
+type RefOf<TComponentName extends keyof Components> = t.static<
+	Components[TComponentName]["refValidator"]
+>;
+
+export { eRef as Ref, RefOf };
